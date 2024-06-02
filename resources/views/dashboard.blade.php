c0f5e8fc1eaf8162150f18cf36c738efc4dfc0c6
Matthias-254 <lannoo.matthias@hotmail.com>
2024-06-02 19:28:52 +0200

    added some style to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,11 +8,26 @@
 
         h2{
             text-align: center;
+            animation: glow 3s infinite;
         }
+
+        .green-text {
+            color: #4caf50;
+        }
+
+        @keyframes glow {
+        0%,100% {
+            text-shadow: none;
+            color: grey;
+        }
+        25%,50% {  
+            color: #4caf50;
+        }
+    }
     </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __("You're logged in!") }}
+            You're logged in!
         </h2>
     </x-slot>
 
@@ -20,7 +35,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100" id="text-in-menu">
-                    {{ __("Welcome to the website of DML motors. Feel free to use the navigation bar at the top to see what else this website has to offer.") }}
+                    <p>Welcome to the website of <span class="green-text">DML motors</span>. Feel free to use the <span class="green-text">navigation bar</span> at the top to see what else this website has to offer.</p>
                 </div>
             </div>
         </div>
