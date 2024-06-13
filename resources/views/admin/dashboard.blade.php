5fbff0872a8a01b6e53c0baf5f30122c765d6b9a
Matthias-254 <lannoo.matthias@hotmail.com>
2024-06-13 23:09:10 +0200

    style geupdate

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,6 +5,7 @@
         }
         h1{
             font-size: 1.5rem;
+            font-weight: bold;
         }
         #boven{
             margin-top:10px;
@@ -23,11 +24,36 @@
         #form{
             float: right;
         }
+        .span{
+            color: whitesmoke;
+            font-weight: bold;
+        }
+        h2{
+            font-size: 1.7rem;
+            font-weight: bold;
+        }
+        #groen{
+            color: #4caf50;
+        }
+        #log{
+            text-align: center;
+            animation: glow 3s infinite;
+        }
+
+        @keyframes glow {
+        0%,100% {
+            text-shadow: none;
+            color: grey;
+        }
+        25%,50% {  
+            color: #4caf50;
+        }
+    }
     </style>
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{("You're logged in admin!")}}
+            <p id="log">You're logged in admin!</p>
         </h2>
     </x-slot>
 
@@ -35,7 +61,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Welcome to the admin Panel! Here you can see info about the website:") }}
+                    <h2>Welcome to the admin Panel! Here you can see <span id="groen">info </span>about the website:</h2>
                 </div>
             </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" id="deel">
@@ -43,8 +69,8 @@
                     <h1>Alle contact verzoeken:</h1>
                     <hr>
                     @foreach ($contact as $contacts)
-                        <p id="boven">Message: {{ $contacts->message }}</p>
-                        <p>From: {{ $contacts->user->email }}</p>
+                        <p id="boven"><span class="span">Message: </span>{{ $contacts->message }}</p>
+                        <p><span class="span">From: </span>{{ $contacts->user->email }}</p>
                         @if(Auth::user()->usertype === 'admin')
                         <form method="POST" action="{{route('contact.destroy', $contacts->id)}}" id="form">
                             @csrf
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,22 +1,50 @@
 <x-app-layout>
     <style>
         #post-del{
-            color: red;
+            border-radius: 5px;
+            background-color: red;
+            padding: 2px 5px;
             float: right;
         }
 
         #post-edit{
-            color: yellowgreen;
+            border-radius: 5px;
+            background-color: #4caf50;
+            padding: 2px 5px;
         }
 
         #post-del:hover{
             cursor: pointer;
         }
+        img{
+            width: 50%;
+            height: auto;
+            margin: 0 auto;
+        }
+        h3{
+            margin-top: 20px;
+            font-size: 1.8rem;
+            font-weight: bold;
+            text-align: center;
+            margin-bottom: 10px;
+        }
+        #content{
+            font-size: 1.1rem;
+            margin-bottom: 10px;
+        }
+        #date{
+            font-size: 0.9rem;
+            margin-bottom: 10px;
+            color: gray;
+        }
+        #groen{
+            color: #4caf50;
+        }
     </style>
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{("Welcome on the news page!")}}
+            <p>Welcome on the <span id="groen">news </span>page!</p>
         </h2>
     </x-slot>
 
@@ -27,14 +55,15 @@
                     @foreach ($posts as $post)
                         <img src="{{ URL($post->image_path) }}" alt="News logo">
                         <h3>{{$post->title}}</h3>
-                        <p>{{$post->content}}</p>
-                        <p>Gepost op {{$post->created_at->format('d/m/y \o\m H:i')}}</p>
+                        <p id="content">{{$post->content}}</p>
+                        <p id="date">Gepost op {{$post->created_at->format('d/m/y \o\m H:i')}}</p>
                         @if(Auth::user()->usertype === 'admin')
-                        <a href="{{ route('posts.edit', $post->id) }}" id="post-edit">Edit POST</a>
+                        <a href="{{ route('posts.edit', $post->id) }}" id="post-edit">Edit Post</a>
                         <form method="POST" action="{{route('posts.destroy', $post->id)}}">
                             @csrf
                             @method('DELETE')
                             <input type="submit" value="Delete Post" id="post-del" onclick="return confirm('delete post?');">
+                            <br>
                         </form>
                         @endif
                         <br><hr><br>
